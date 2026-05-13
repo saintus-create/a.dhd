2967611c709991b7abfe0511e773b4c1a3ad55b8
saintus-create <bradleydip@gmail.com>
2026-05-12 19:57:42 -0700

    chore: flatten project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import app from "./app.js";
+import { logger } from "./lib/logger.js";
+
+const port = Number(process.env.PORT ?? 3000);
+
+app.listen(port, (err) => {
+  if (err) {
+    logger.error({ err }, "Error listening on port");
+    process.exit(1);
+  }
+  logger.info({ port }, "Server listening");
+});
